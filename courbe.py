29cb352bba50e7c0aa1fee235b9e463061d2c1be
GregHsr <gregoire.husser@etu.inp-n7.fr>
2023-11-15 14:15:33 +0100

    avancée

--- courbe.py
+++ courbe.py
@@ -2,6 +2,20 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 
+# Fonction longueur de mélange
+
+def u_moy_long_mel(u_et,l_et,y,h,Re_et):
+    return u_et*(((np.log(y/h)+np.log(Re_et))/kappa) +5)
+
+def visco_turb_mel(y, du_dy):
+    return kappa*kappa*y*y*du_dy
+
+# Variables
+
+kappa = 0.384
+l_et = 1.92*10**(-5)
+u_et = 0.78
+
 ### Lecture des fichiers csv et xy
 
 def read_xy(filename):
@@ -120,40 +134,125 @@ u_effectif = [u_fichier*0.78 for u_fichier in U]
 
 mag_vel_fluent,y_vel_fluent = read_xy("vitesse_moyenne.xy")
 mag_vel_star,y_vel_star = read_csv("ux.csv", 2) 
-plt.figure(1)
-plt.plot(mag_vel_fluent,y_vel_fluent, label="Fluent")
-plt.plot(mag_vel_star,y_vel_star, label="Star-CCM+")
-plt.plot(U,y_effectif,label="DNS")
-plt.ylabel("y (m)")
-plt.xlabel("Vitesse (m/s)")
-plt.legend()
+#plt.figure(1)
+#plt.plot(mag_vel_fluent,y_vel_fluent, label="Fluent "+r"$K-\epsilon$")
+#plt.plot(mag_vel_star,y_vel_star, label="Star-CCM+ "+r"$K-\epsilon$")
+#plt.plot(u_effectif,y_effectif,label="DNS")
+#plt.ylabel("y (m)")
+#plt.xlabel("Vitesse (m/s)")
+#plt.legend()
+
+# avec yplus
+
+#plt.figure(3)
+#plt.subplot(121)
+#plt.plot(mag_vel_fluent,[k/l_et for k in y_vel_fluent], label="Fluent "+r"$K-\epsilon$")
+#plt.plot(mag_vel_star,[k/l_et for k in y_vel_star], label="Star-CCM+ "+r"$K-\epsilon$")
+#plt.plot(u_effectif,yplus,label="DNS")
+#plt.plot([u_moy_long_mel(u_et,l_et,y,0.1,5200) for y in y_effectif],yplus,label="Longueur de mélange")
+#plt.ylabel("y+")
+#plt.xlabel("Vitesse (m/s)")
+#plt.legend()
+#plt.subplot(122)
+#plt.plot(mag_vel_fluent,[k/l_et for k in y_vel_fluent], label="Fluent "+r"$K-\epsilon$")
+#plt.plot(mag_vel_star,[k/l_et for k in y_vel_star], label="Star-CCM+ "+r"$K-\epsilon$")
+#plt.plot(u_effectif,yplus,label="DNS")
+#plt.plot([u_moy_long_mel(u_et,l_et,y,0.1,5200) for y in y_effectif],yplus,label="Longueur de mélange")
+#plt.ylabel("y+")
+#plt.xlabel("Vitesse (m/s)")
+#plt.legend()
+#plt.yscale('log')
+#plt.grid(which='both')
+
+# Profils avec y+ et u+
+
+#plt.figure(6)
+#plt.subplot(121)
+#plt.plot([k/u_et for k in mag_vel_fluent],[k/l_et for k in y_vel_fluent], label="Fluent "+r"$K-\epsilon$")
+#plt.plot([k/u_et for k in mag_vel_star],[k/l_et for k in y_vel_star], label="Star-CCM+ "+r"$K-\epsilon$")
+#plt.plot([k/u_et for k in U],yplus,label="DNS")
+#plt.plot([k/u_et for k in [u_moy_long_mel(u_et,l_et,y,0.1,5200) for y in y_effectif]],yplus,label="Longueur de mélange")
+#plt.ylabel("y+")
+#plt.xlabel("u+")
+#plt.legend()
+#plt.subplot(122)
+#plt.plot([k/u_et for k in mag_vel_fluent],[k/l_et for k in y_vel_fluent], label="Fluent "+r"$K-\epsilon$")
+#plt.plot([k/u_et for k in mag_vel_star],[k/l_et for k in y_vel_star], label="Star-CCM+ "+r"$K-\epsilon$")
+#plt.plot(U,yplus,label="DNS")
+#plt.plot([k/u_et for k in [u_moy_long_mel(u_et,l_et,y,0.1,5200) for y in y_effectif]],yplus,label="Longueur de mélange")
+#plt.ylabel("y+")
+#plt.xlabel("u+")
+#plt.legend()
+#plt.yscale('log')
+#plt.grid(which='both')
 
 # Etablissment de la vitesse
 
 x1,x2,x3,x4,x5,x6,x7,x8,x9,x10,x11,x12 = read_csv("section_velocity(1).csv", 12)
-plt.figure(2)
-plt.plot(x1,x2,'o', label="x=0m")
-plt.plot(x3,x4,'o', label="x=4m")
-plt.plot(x5,x6,'o', label="x=6m")
-plt.plot(x7,x8,'o', label="x=7m")
-plt.plot(x11,x12,'o', label="x=7.5m")
-plt.plot(x9,x10,'o', label="x=8m")
-plt.ylabel("y (m)") 
-plt.xlabel("Vitesse (m/s)")
-plt.legend()
+#plt.figure(2)
+#plt.plot(x1,x2,'o', label="x=0m")
+#plt.plot(x3,x4,'o', label="x=4m")
+#plt.plot(x5,x6,'o', label="x=6m")
+#plt.plot(x7,x8,'o', label="x=7m")
+#plt.plot(x11,x12,'o', label="x=7.5m")
+#plt.plot(x9,x10,'o', label="x=8m")
+#plt.ylabel("y (m)") 
+#plt.xlabel("Vitesse (m/s)")
+#plt.legend()
 
 
 # Energie cinétique turbulente
 
 mag_k_fluent,y_k_fluent = read_xy("nrj_cin_turb.xy")
 mag_k_star,y_k_star = read_csv("turbulent_kinetic_energy.csv", 2)
-plt.figure(3)
-plt.plot(mag_k_fluent,y_k_fluent, label="Fluent")
-plt.plot(mag_k_star,y_k_star, label="Star-CCM+")
-plt.plot(k,y_effectif,label="DNS")
-plt.ylabel("y (m)")
-plt.xlabel("Energie cinétique turbulente (m²/s²)")
-plt.legend()
 
+# Energie cinétique turbulente vec y+
+
+y_plus_k_fluent = [y/(1.92*10**(-5)) for y in y_k_fluent]
+y_plus_k_star = [y/(1.92*10**(-5)) for y in y_k_star]
+
+#plt.figure(4)
+#plt.plot(mag_k_fluent,y_plus_k_fluent, label="Fluent")
+#plt.plot(mag_k_star,y_plus_k_star, label="Star-CCM+")
+#plt.plot([0.5*k for k in uu],yplus,label="DNS")
+#plt.ylabel("y+")
+#plt.xlabel("Energie cinétique turbulente (m²/s²)")
+#plt.legend()
+
+# Ylog
+#plt.figure(5)
+#plt.plot(mag_k_fluent,y_plus_k_fluent, label="Fluent")
+#plt.plot(mag_k_star,y_plus_k_star, label="Star-CCM+")
+#plt.plot([0.5*k for k in uu],yplus,label="DNS")
+#plt.ylabel("y+")
+#plt.xlabel("Energie cinétique turbulente (m²/s²)")
+#plt.legend()
+#plt.yscale('log')
+#plt.grid(which='both')
+
+# Viscosité turbulente 
+
+visco_k_fluent,y_k_fluent = read_xy("visc_turb.xy")
+visco_k_star,y_k_star = read_csv("turbulent_viscosity.csv", 2)
+
+# Dissipation et production d'énergie cinétique turbulente
+
+dissip_k_fluent,y_k_fluent = read_xy("taux_dissipation.xy")
+dissip_k_star,y_k_star = read_csv("turbulent_dissipation_rate.csv", 2)
+
+prod_k_fluent,y_k_fluent = read_xy("taux_production.xy")
+
+# Energie cinétique turbulente avec y+
+
+y_plus_k_fluent = [y/(1.92*10**(-5)) for y in y_k_fluent]
+y_plus_k_star = [y/(1.92*10**(-5)) for y in y_k_star]
+
+plt.figure(7)
+plt.plot(visco_k_fluent,y_plus_k_fluent, label="Fluent "+r"$K-\epsilon$")
+plt.plot(visco_k_star,y_plus_k_star, label="Star-CCM+ "+r"$K-\epsilon$")
+plt.plot(visc_dissip,yplus,label="DNS")
+plt.ylabel("y+")
+plt.xlabel("Viscosité turbulente (m²/s)")
+plt.legend()
 
 plt.show()
